60f7f16015750e9cf75e93e77300b316b4c93e9e
isaaconek <onekdev5@gmail.com>
2025-04-12 11:04:14 +0100

    Installation de chadcn et création du formaulaire de connexion

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,23 @@ module.exports = {
       "./components/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-      extend: {},
+      extend: {
+        backgroundColor : {
+            principale: '#00693E',
+            aliceblue: '#F0F8FF',
+            jaune: '#F4C431',
+            'main-green': '#00693E',
+            'secondary-green': '#7ED218'
+        },
+        screens: {
+            'xs': { max: '639px' },
+            'tablet': { min: '640px', max: '1023px' },
+            'desktop': { min: '1024px' },
+          },
+          colors: {
+            mainred:"#FF0800",
+          }
+      },
     },
     plugins: [],
   }
