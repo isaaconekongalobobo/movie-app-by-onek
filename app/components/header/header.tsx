8098e794a01093e91b61bd125cf1ff0ec83e9c14
isaaconek <onekdev5@gmail.com>
2025-04-13 02:19:38 +0100

    gestion de la deconnexion avec clerk

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -13,24 +13,24 @@ const ongletList: OngletType[] = [
     { href: "/playist", text: "Ma playlist" },
 ]
 const Header = () => {
+    const user = useUser()
     const userConnected = useUser().user
     const imageProfile = userConnected?.imageUrl ? userConnected?.imageUrl : '/images/userConnectedIcon.png'
-    const [ showContainer, setShowContainer ] = useState(false)
-    const hiddeContainer = () => setShowContainer (false)
+    const [ showProfileContainer, setShowProfileContainer ] = useState(false)
 
     return (
-        <header className="bg-[#010B13] flex justify-between px-4 sm:px-8 py-3 sm:py-5 items-center  " >
+        <header className={`bg-[#010B13] flex justify-between px-4 sm:px-8 py-3 sm:py-5 items-center ${user.isSignedIn == false && 'hidden'}`} >
             <h1 className='sm:text-xl font-semibold text-[#FF0800] '>Movie App <mark className='bg-white text-[#FF0800] sm:px-4 px-1 py-1 font-bold sm:text-2xl rounded '>by Onek</mark></h1>
             <nav className=" hidden sm:flex flex-row justify-center gap-16 w-2/4  ">
                 {
                     ongletList.map ((onglet, key) => <Onglet href={onglet.href} text={onglet.text}/> )
                 }  
             </nav>  
-            <div className='flex flex-row justify-center gap-2 items-center' onClick={() => setShowContainer (true)}>
+            <div className='flex flex-row justify-center gap-2 items-center cursor-pointer' title='Votre profile' onClick={() => setShowProfileContainer (true)}>
                 <div className="bg-center bg-cover size-8 sm:size-10 rounded-full" style={{ backgroundImage: `url(${imageProfile})` }}/>
                 <span> {userConnected?.fullName} </span>
             </div>
-            <UserProfileContainer showContainer={showContainer} hiddeContainer={hiddeContainer}  />     
+            { showProfileContainer && user.isSignedIn  && <UserProfileContainer showContainer={showProfileContainer} setShowContainer={setShowProfileContainer}/>  }
         </header>
 
     );
--- app/components/userProfile/userProfileContainer.tsx
+++ app/components/userProfile/userProfileContainer.tsx
@@ -1,19 +1,49 @@
+"use client"
+
 import React from 'react';
 import { MdOutlineCloseFullscreen } from "react-icons/md";
+import { motion } from 'framer-motion';
+import { useAuth, useUser } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
 
 interface userProfileContainerType {
     showContainer: boolean,
-    hiddeContainer: () => void
+    setShowContainer: React.Dispatch<React.SetStateAction<boolean>>
 }
 
-const UserProfileContainer = ({showContainer, hiddeContainer} : userProfileContainerType) => {
+const containerVariant = {
+    initial: {
+        y: -10,
+        opacity: 0
+    },
+    animate: {
+        y: 0,
+        opacity: 1
+    }
+}
+
+const UserProfileContainer = ({showContainer, setShowContainer} : userProfileContainerType) => {
+    const userConnected = useUser().user
+    const { signOut } = useAuth()
+    const SignOut = async () => {
+        setShowContainer(false)
+        await signOut().then(() => {
+            redirect ("/")
+        }).catch (() => {
+            alert ("Une erreur c'est produite lors de la déconnexion")
+        })
+    }
     return (
-        <div className='h-40 w-60 p-4 bg-white absolute top-24 right-6 text-black rounded '>
-            <header className='flex items-center justify-between '>
+        <motion.div variants={containerVariant} initial="initial" animate="animate"  className=' w-60 p-4 bg-white absolute top-24 right-6 text-black rounded border-2 hover:border-[#FF0800] flex flex-col items-center justify-center gap-4 '>
+            <header className='flex items-center justify-between sm:gap-24 '>
                 <h1>Votre profile</h1>
-                <MdOutlineCloseFullscreen onAuxClick={hiddeContainer} />
+                <MdOutlineCloseFullscreen onClick={() => setShowContainer (false)}  />
             </header>
-        </div>
+            <main>
+                <div className="bg-center bg-cover size-8 sm:size-52 rounded" style={{ backgroundImage: `url(${userConnected?.imageUrl})` }}/>
+            </main>
+            <button className='bg-[#FF0800] hover:bg-[#FF0800CC] hover:text-white px-8 py-2 rounded-sm font-semibold cursor-pointer' onClick={() => signOut()}>Se deconnecter</button>
+        </motion.div>
     );
 }
 
