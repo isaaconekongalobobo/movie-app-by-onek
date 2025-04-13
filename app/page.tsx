b25a85eb74486b836f72b4bc71fd3ca7f375608f
isaaconek <onekdev5@gmail.com>
2025-04-13 02:28:21 +0100

    Gestion de la responsivité dans la page de connexion

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="flex justify-center flex-col items-center h-screen bg-[#FF0800] ">
       <main className="flex flex-col gap-5">
-        <h1 className="text-3xl font-semibold text-white">The Movie app <mark className="font-sans px-8 bg-[#010B13] rounded text-white ">by Onek</mark> </h1>
+        <h1 className=" text-2xl sm:text-3xl font-semibold text-white">The Movie app <mark className="font-sans px-8 bg-[#010B13] rounded text-white ">by Onek</mark> </h1>
         <LoginForm/>
       </main>
     </div>
