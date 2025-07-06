8bb26df9acdf98c2472a8d1b0f2539418916c7eb
isaaconek <onekdev5@gmail.com>
2025-07-06 10:42:54 +0100

    Ajout de la persistance de donnée pour le store du user connecté

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { neon } from '@neondatabase/serverless';
 
 export default function Home() {
   return (
-    <div className="flex justify-center flex-col items-center h-screen bg-[#FF0800] ">
+    <div className="flex justify-center flex-col items-center h-screen bg-[#FF0800]">
       <main className="flex flex-col gap-5">
         <h1 className=" text-2xl sm:text-3xl font-semibold text-white">The Movie app <mark className="font-sans px-8 bg-[#010B13] rounded text-white ">by Onek</mark> </h1>
         <LoginForm/>
--- app/store/userConnected.ts
+++ app/store/userConnected.ts
@@ -1,7 +1,16 @@
 import { create } from 'zustand'
+import { persist, createJSONStorage } from 'zustand/middleware'
 import { User, UserConnectedStore } from '../types/allTypes'
 
-export const useUserConnected = create<UserConnectedStore>((set) => ({
-    userConnected: null,
-    setUserConnected: (autheticatedUser: User | null) => set({ userConnected: autheticatedUser })
-}))
+export const useUserConnected = create<UserConnectedStore>()(
+  persist(
+    (set) => ({
+      userConnected: null,
+      setUserConnected: (authenticatedUser: User | null) => set({ userConnected: authenticatedUser }),
+    }),
+    {
+      name: 'user-connected-store',
+      storage: createJSONStorage(() => sessionStorage),
+    }
+  )
+)
